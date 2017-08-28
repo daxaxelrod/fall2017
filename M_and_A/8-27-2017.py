5c9d1db62618845da55fa4171c93e93e955da796
David Axelrod <daxaxelrod@gmail.com>
2017-08-28 09:50:42 -0400

    first commit. M&A class up to 8-28-17

---
+++ M_and_A/8-27-2017.py
@@ -0,0 +1,37 @@
+M and A course
+
+Difference between M&A
+3 types
+    Horizontal
+        merging with a company that has similar or compatible product lines
+        common in mature industries
+        Goals:
+            Increased market share
+            Economies of scale
+    Vertical
+        Combine within same value chain
+        Aquire suppplier - backward integration
+    Conglomerate
+        random unrelated companies
+        decreases risk and diversify
+
+So why do firms merge?
+    Right reasons to do a deal
+        Synergies
+            Earn more money than you otherwise would
+        To get big fast
+        To fill out a product line
+        To expand geographically
+        To increase market share and market power
+    Not so great reasons
+        To diversify
+        For tax reasons
+        To expand debt capacity
+        Management self interest
+        Capitalize on underutilized assets
+        Look for companies with faster growth, better technology and better margins
+
+Types of Synergies
+    Revenue enhancements
+    cost savings
+    Maybe even financial engineering
